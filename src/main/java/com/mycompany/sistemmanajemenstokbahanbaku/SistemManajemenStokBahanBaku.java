c16602b548828387e5aad1b0eed9c3bd413b44cf
anantahanif <anantahanif03@gmail.com>
2026-09-21 23:02:13 +0800

    Initial commit - Clean project structure

---
+++ src/main/java/com/mycompany/sistemmanajemenstokbahanbaku/SistemManajemenStokBahanBaku.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemmanajemenstokbahanbaku;
+
+/**
+ *
+ * @author HYPE R Series
+ */
+public class SistemManajemenStokBahanBaku {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
